c9be7d618d2fbb6abe96664b125dcc9149f82294
Glassgiver <alfiya.usmanova.2004@gmail.com>
2024-04-26 11:00:15 +0500

    finished

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,26 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+        Employee[] employees = new Employee[5];
+        employees[0] = new Employee("Boris", "Boriska", "Driver", "borisBoriska@gmail.com",
+                "88083737353", 2000, 45);
+        employees[1] = new Employee("Anna", "Annushka", "Manager", "annaAnnushka@gmail.com",
+                "88083556589", 3000, 58);
+        employees[2] = new Employee("Igor", "Igorek", "Developer", "igorIgorek@gmail.com",
+                "88083454667", 4000, 32);
+        employees[3] = new Employee("Olga", "Olgushka", "Accountant", "olgaOlgushka@gmail.com",
+                "88083325544", 2500, 41);
+        employees[4] = new Employee("Sergey", "Serega", "Designer", "sergeySerega@gmail.com",
+                "88083224455", 3500, 29);
+
+        for (Employee i: employees) {
+            if(i.getAge() >= 40){
+                System.out.println("\n---");
+                i.showInfo();
+            }
+        }
+
     }
 }
--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,2 +1,37 @@
-package org.example;public class Employee {
+package org.example;
+
+public class Employee {
+
+    private String name;
+    private String surname;
+    private String position;
+    private String email;
+    private String phone_number;
+    private int salary;
+    private int age;
+
+    public Employee(){
+
+    }
+
+    public Employee(String name, String surname, String position, String email, String phone_number, int salary, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.position = position;
+        this.email = email;
+        this.phone_number = phone_number;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void showInfo(){
+        System.out.printf("Employee info: " +
+                "\n name: %s" + "\n surname: %s" + "\n position: %s" + "\n email: %s" + "\n phone number: %s" + "\n salary: %d" + "\n age: %d",
+                name, surname, position, email, phone_number, salary, age);
+
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
